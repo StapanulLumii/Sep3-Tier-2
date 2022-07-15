c04b5e254f6ba2e1632f5f551e685cfbc8dda31d
StapanulLumii <82084902+StapanulLumii@users.noreply.github.com>
2022-07-15 07:41:50 +0200

    IGame interface

--- src/IGame.java
+++ src/IGame.java
@@ -1,3 +1,23 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public interface IGame {
 
+    ArrayList<Game> Games = new ArrayList<Game>();
+
+    public Games AddId(int id);
+
+    public Games RemoveId(int id);
+    public Games AddName(String Name);
+    public Games RemoveName(String Name);
+    public Games AddDescription(String Description);
+    public Games RemoveDescription(String Description);
+    public Games AddImage();
+    public Games RemoveImage();
+    public Games AddPrice(double Price);
+    public Games RemovePrice(double Price);
+    public Games AddCategory();
+    public Games RemoveCategory();
+
+    public  Games GetGames();
 }
